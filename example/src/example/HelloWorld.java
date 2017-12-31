b0050bd00b3c9e0fe1cd21e8f4c3418fde07fe9c
souparno majumder <souparno.majumder@gmail.com>
2017-12-31 22:31:46 +0530

    example files separated into a different folder

---
+++ example/src/example/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+/**
+ *
+ * @author bonnie
+ */
+public class HelloWorld {
+    
+    public static void main (String args[]) {
+      int a =12;
+      int b = 15;
+      int c = a + b;
+      
+      System.out.println(c);
+    }
+    
+}
